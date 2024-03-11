30fd74c5c38bbab71cdceddb81c2f95be82a8575
Chase Sterling <chase.sterling@gmail.com>
2024-03-10 22:54:15 -0400

    turn off caps word invert shift and customize caps word chars

--- keyboards/voyager/keymaps/gaz/keymap.c
+++ keyboards/voyager/keymaps/gaz/keymap.c
@@ -273,3 +273,23 @@ void leader_end_user(void) {
         SEND_STRING("chase.and.rachel.sterling@gmail.com");
     }
 }
+
+bool caps_word_press_user(uint16_t keycode) {
+    switch (keycode) {
+        // Keycodes that continue Caps Word, with shift applied.
+        case KC_A ... KC_Z:
+            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+            return true;
+
+        // Keycodes that continue Caps Word, without shifting.
+        case KC_1 ... KC_0:
+        case KC_BSPC:
+        case KC_DEL:
+        case KC_UNDS:
+        case KC_MINS:
+            return true;
+
+        default:
+            return false;  // Deactivate Caps Word.
+    }
+}
